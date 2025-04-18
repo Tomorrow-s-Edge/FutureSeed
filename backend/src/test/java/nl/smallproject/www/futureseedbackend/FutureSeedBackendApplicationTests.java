02ea45059639d0c8b42c3f40eef38af156df5f57
smallproject <j.a.villas@live.com.ph>
2025-04-18 12:32:07 +0200

    Re-added backend as a normal folder

---
+++ backend/src/test/java/nl/smallproject/www/futureseedbackend/FutureSeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.smallproject.www.futureseedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureSeedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
