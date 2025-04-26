3b642dc4fd96d9d4790d15cb0354faa5d7d0db7d
smallproject <j.a.villas@live.com.ph>
2025-04-26 08:05:19 +0200

    added create subject in subject controller

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/controllers/SubjectController.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/controllers/SubjectController.java
@@ -1,16 +1,21 @@
 package nl.smallproject.www.futureseedbackend.controllers;
 
+import nl.smallproject.www.futureseedbackend.dtos.SubjectInputDto;
 import nl.smallproject.www.futureseedbackend.dtos.SubjectOutputDto;
 import nl.smallproject.www.futureseedbackend.services.SubjectService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/addresses")
+@RequestMapping("/api/v1/subjects")
 public class SubjectController {
 
     private final SubjectService subjectService;
@@ -23,5 +28,18 @@ public class SubjectController {
     public ResponseEntity<List<SubjectOutputDto>> getAllSubjects() {
         List<SubjectOutputDto> subjectOutputDtos = subjectService.getAllSubjects();
         return ResponseEntity.ok(subjectOutputDtos);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Object> createSubject(@RequestBody SubjectInputDto subjectInputDto) {
+        var newSubject = subjectService.createSubject(subjectInputDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/" + newSubject.getId())
+                .buildAndExpand(newSubject)
+                .toUri();
+
+        return ResponseEntity.created(location).eTag(String.valueOf(HttpStatus.CREATED)).body(newSubject);
     }
 }
