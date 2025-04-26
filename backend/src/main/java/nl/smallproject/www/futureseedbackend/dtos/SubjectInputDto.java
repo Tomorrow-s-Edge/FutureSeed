99ee73a693ca04050432beea6169c46da48fa4b0
smallproject <j.a.villas@live.com.ph>
2025-04-26 08:03:06 +0200

    added subject input dto

---
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/dtos/SubjectInputDto.java
@@ -0,0 +1,33 @@
+package nl.smallproject.www.futureseedbackend.dtos;
+
+import java.util.List;
+
+public class SubjectInputDto {
+    private String name;
+    private String description;
+    private List<String> topics;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<String> topics) {
+        this.topics = topics;
+    }
+}
