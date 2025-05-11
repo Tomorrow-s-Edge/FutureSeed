ac6260c1ad5b3ecbb6a22cdb2a23b61deb23c103
smallproject <j.a.villas@live.com.ph>
2025-05-11 08:38:44 +0200

    added a database action to get all users

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
@@ -2,10 +2,14 @@ package nl.smallproject.www.futureseedbackend.services;
 
 import jakarta.transaction.Transactional;
 import nl.smallproject.www.futureseedbackend.dtos.user.UserInputOrUpdatedto;
+import nl.smallproject.www.futureseedbackend.dtos.user.UserOutputDto;
 import nl.smallproject.www.futureseedbackend.mappers.UserMapper;
 import nl.smallproject.www.futureseedbackend.models.User;
 import nl.smallproject.www.futureseedbackend.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -22,5 +26,14 @@ public class UserService {
         User user = userMapper.userInputDtoToEntity(userInputDto);
         userRepository.save(user);
         return user;
+    }
+
+    public List<UserOutputDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserOutputDto> userOutputDtos = new ArrayList<>();
+        for (User user : users) {
+            userOutputDtos.add(userMapper.userEntityToOutputDto(user));
+        }
+        return userOutputDtos;
     }
 }
