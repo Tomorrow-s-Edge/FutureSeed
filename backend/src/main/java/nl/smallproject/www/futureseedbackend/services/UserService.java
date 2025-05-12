537e56c5965e4c31fcb602a435df8fe77d6fc81c
smallproject <j.a.villas@live.com.ph>
2025-05-12 22:26:34 +0200

    added getUserById functionality

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
@@ -10,8 +10,10 @@ import nl.smallproject.www.futureseedbackend.repositories.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,13 +25,6 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
-    @Transactional
-    public User CreateUser(UserInputOrUpdatedto userInputDto) {
-        User user = userMapper.userInputDtoToEntity(userInputDto);
-        userRepository.save(user);
-        return user;
-    }
-
     public List<UserOutputDto> getAllUsers() {
         List<User> users = userRepository.findAll();
         List<UserOutputDto> userOutputDtos = new ArrayList<>();
@@ -37,6 +32,26 @@ public class UserService {
             userOutputDtos.add(userMapper.userEntityToOutputDto(user));
         }
         return userOutputDtos;
+    }
+
+    @Transactional
+    public UserOutputDto getUserById(String id) {
+        Optional<User> userOptional = Optional.ofNullable(userRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("User not found with this id: " + id)));
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return userMapper.userEntityToOutputDto(user);
+        } else {
+            throw new RecordNotFoundException("User not found with this id: " + id);
+        }
+    }
+
+    @Transactional
+    public User CreateUser(UserInputOrUpdatedto userInputDto) {
+        User user = userMapper.userInputDtoToEntity(userInputDto);
+        userRepository.save(user);
+        return user;
     }
 
     @Transactional
