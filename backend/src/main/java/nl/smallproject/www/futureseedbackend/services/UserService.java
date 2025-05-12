845ac77bcb20e8be6e79072d0eb0657e6dad0e91
smallproject <j.a.villas@live.com.ph>
2025-05-12 22:13:10 +0200

    added update database action

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/services/UserService.java
@@ -3,9 +3,11 @@ package nl.smallproject.www.futureseedbackend.services;
 import jakarta.transaction.Transactional;
 import nl.smallproject.www.futureseedbackend.dtos.user.UserInputOrUpdatedto;
 import nl.smallproject.www.futureseedbackend.dtos.user.UserOutputDto;
+import nl.smallproject.www.futureseedbackend.exceptions.RecordNotFoundException;
 import nl.smallproject.www.futureseedbackend.mappers.UserMapper;
 import nl.smallproject.www.futureseedbackend.models.User;
 import nl.smallproject.www.futureseedbackend.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -35,5 +37,15 @@ public class UserService {
             userOutputDtos.add(userMapper.userEntityToOutputDto(user));
         }
         return userOutputDtos;
+    }
+
+    @Transactional
+    public void updateUser(String id, UserInputOrUpdatedto userUpdateDto) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("User not found with this id: " + id));
+
+        User updatedUser = userMapper.userInputDtoToEntity(userUpdateDto);
+        BeanUtils.copyProperties(updatedUser, existingUser, "id");
+        userRepository.save(existingUser);
     }
 }
