35a8b4ba43e53b86845a67978bb0878d4517b5b7
smallproject <j.a.villas@live.com.ph>
2025-05-21 12:44:26 +0200

    implemented lombok in student

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/models/Student.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/models/Student.java
@@ -2,10 +2,14 @@ package nl.smallproject.www.futureseedbackend.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity(name = "students")
 public class Student extends BaseObject{
     private String studentNumber;
@@ -20,63 +24,5 @@ public class Student extends BaseObject{
 
     public Student() {
         super();
-        this.studentNumber = UUID.randomUUID().toString();
-        this.enrollmentDate = LocalDateTime.now();
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getStudentNumber() {
-        return studentNumber;
-    }
-
-    public void setStudentNumber(String studentNumber) {
-        this.studentNumber = studentNumber;
-    }
-
-    public String getGradeLevel() {
-        return gradeLevel;
-    }
-
-    public void setGradeLevel(String gradeLevel) {
-        this.gradeLevel = gradeLevel;
-    }
-
-    public LocalDateTime getEnrollmentDate() {
-        return enrollmentDate;
-    }
-
-    public void setEnrollmentDate(LocalDateTime enrollmentDate) {
-        this.enrollmentDate = enrollmentDate;
-    }
-
-    public String getClassGroup() {
-        return classGroup;
-    }
-
-    public void setClassGroup(String classGroup) {
-        this.classGroup = classGroup;
-    }
-
-    public String getGuardianName() {
-        return guardianName;
-    }
-
-    public void setGuardianName(String guardianName) {
-        this.guardianName = guardianName;
-    }
-
-    public String getGuardianContact() {
-        return guardianContact;
-    }
-
-    public void setGuardianContact(String guardianContact) {
-        this.guardianContact = guardianContact;
     }
 }
