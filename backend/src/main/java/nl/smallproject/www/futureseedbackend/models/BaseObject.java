962555b4378d8db111e044806d29e21078656f48
smallproject <j.a.villas@live.com.ph>
2025-05-21 12:42:25 +0200

    implemented lombok in base

--- backend/src/main/java/nl/smallproject/www/futureseedbackend/models/BaseObject.java
+++ backend/src/main/java/nl/smallproject/www/futureseedbackend/models/BaseObject.java
@@ -1,17 +1,20 @@
 package nl.smallproject.www.futureseedbackend.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseObject {
 
     @Id
-    protected String id;
+    @GeneratedValue
+    protected UUID id;
 
     @Column(name = "created_at")
     protected LocalDateTime created_At;
@@ -19,37 +22,14 @@ public abstract class BaseObject {
     @Column(name = "updated_at")
     protected LocalDateTime updated_At;
 
-    public BaseObject() {
-        this.id = UUID.randomUUID().toString();
+    @PrePersist
+    protected void onCreate() {
         this.created_At = LocalDateTime.now();
         this.updated_At = LocalDateTime.now();
     }
 
-    public void save() {
+    @PreUpdate
+    protected void onUpdate() {
         this.updated_At = LocalDateTime.now();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreated_At() {
-        return created_At;
-    }
-
-    public void setCreated_At(LocalDateTime created_At) {
-        this.created_At = created_At;
-    }
-
-    public LocalDateTime getUpdated_At() {
-        return updated_At;
-    }
-
-    public void setUpdated_At(LocalDateTime updated_At) {
-        this.updated_At = updated_At;
     }
 }
