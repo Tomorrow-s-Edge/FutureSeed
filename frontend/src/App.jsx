b3d7487526ed5df3aed7cc35750595213045c8ea
smallproject <j.a.villas@live.com.ph>
2025-09-30 22:03:05 +0200

    renamed for scalable approach

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Login from "./authentication/login/Login.jsx";
-import Homepage from "./pages/homepage/Homepage.jsx";
+import Home from "./pages/HomePage/index.jsx";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <>
         <main>
             <Routes>
-                <Route path="/" element={<Homepage/>}/>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
             </Routes>
         </main>
